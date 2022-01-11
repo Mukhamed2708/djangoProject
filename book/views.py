fecf0be0dd9ef58a08183351769037b5908ce4b8
Mukhamed2708 <96417043+Mukhamed2708@users.noreply.github.com>
2022-01-11 16:18:00 +0600

    first_hw

--- book/views.py
+++ book/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from . import models
 
+
 def hello_world(request):
     return HttpResponse('Hello World')
 
 
-def blog_all(reqeuest):
-    post = models.Post.objects.all()
-    return render(reqeuest, 'post_list.html', {'post': post})
+def book_all(request):
+    genre = models.Genre.objects.all()
+    return render(request, 'genre_list.html', {'genre': genre})
